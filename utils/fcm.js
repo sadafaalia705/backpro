f66a3daf46a88144ff4fc8ef26df62ff9260600a
Neha Sindhwani <nehasindhwani01@gmail.com>
2025-07-24 18:54:01 +0530

    jdnkjf

--- utils/fcm.js
+++ utils/fcm.js
@@ -5,9 +5,9 @@ import admin from 'firebase-admin';
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      projectId: process.env.project_id,
+      privateKey: process.env.private_key.replace(/\\n/g, '\n'),
+      clientEmail: process.env.client_email,
     }),
   });
 }
